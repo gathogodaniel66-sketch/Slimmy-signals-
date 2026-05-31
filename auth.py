bbcfc61e343ee59ca725f19d88e509d6c4f4c163
gathogodaniel66-sketch <gathogodaniel66@gmail.com>
2026-05-31 17:31:09 +0300

    Implement user login with Streamlit

--- auth.py
+++ auth.py
@@ -1,1 +1,20 @@
-# Authentication system
+import streamlit as st
+
+def login():
+
+    st.sidebar.title("User Login")
+
+    username = st.sidebar.text_input(
+        "Username"
+    )
+
+    password = st.sidebar.text_input(
+        "Password",
+        type="password"
+    )
+
+    if st.sidebar.button("Login"):
+
+        st.success(
+            f"Welcome {username}"
+        )
